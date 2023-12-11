a819d772756b3a1e67cb300656c9639dcffc52b6
Carrie Oliver <carrieoliver@googlemail.com>
2023-12-11 20:33:46 +0000

    Day 9 solution

--- AdventOfCode2023/Day9.cs
+++ AdventOfCode2023/Day9.cs
@@ -9,11 +9,49 @@ namespace AdventOfCode
         }
 
         public string SolveA () {
-            return "A";
+            var nextNumbers = new List<int>();
+            foreach (var line in Lines) {
+                var nextSequence = line.Split(' ').Select(x => int.Parse(x)).ToList();
+                nextNumbers.Add(FindNextInSequence(nextSequence));
+            }
+
+            return nextNumbers.Sum().ToString();
         }
 
         public string SolveB () {
-            return "B";
+            var nextNumbers = new List<int>();
+            foreach (var line in Lines) {
+                var nextSequence = line.Split(' ').Select(x => int.Parse(x)).ToList();
+                nextNumbers.Add(FindPreviousInSequence(nextSequence));
+            }
+            
+            return nextNumbers.Sum().ToString();
+        }
+
+        private int FindNextInSequence(List<int> currentSequence) {
+            var difference = new List<int>();
+            for (int i = 1; i < currentSequence.Count; i++) {
+                difference.Add(currentSequence[i] - currentSequence[i-1]);
+            }
+
+            if (difference.Distinct().Count() > 1) {
+                return currentSequence.Last() + FindNextInSequence(difference);
+            }
+
+            return currentSequence.Last() + difference.Distinct().Single();
+        }
+
+        private int FindPreviousInSequence(List<int> currentSequence) {
+            var difference = new List<int>();
+            for (int i = 1; i < currentSequence.Count; i++) {
+                difference.Add(currentSequence[i] - currentSequence[i-1]);
+            }
+
+            if (difference.Distinct().Count() > 1) {
+                return currentSequence.First() - FindPreviousInSequence(difference);
+            }
+
+            return currentSequence.First() - difference.Distinct().Single();
         }
     }
 }
