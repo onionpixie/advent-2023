47d4dda3d4e60b2bc7ef732dfcd7db2a8dc2c6fa
Carrie Oliver <carrieoliver@googlemail.com>
2023-12-23 18:40:01 +0000

    wip day 14

--- AdventOfCode2023/Day14.cs
+++ AdventOfCode2023/Day14.cs
@@ -86,7 +86,136 @@ namespace AdventOfCode
         }
 
         public string SolveB () {
-            return "B";
+            var rows = Lines.Length;
+            var columns = Lines.First().Length;
+
+            var totalRockWeight = 0;
+            var previousRolledMatrix = GetInitalMatrix(rows, columns);
+            for (var m = 0; m < 1000000000; m++) {
+                if (m != 0) {
+                    previousRolledMatrix = GoNorth(rows, columns, previousRolledMatrix);
+                }
+                //previousRolledMatrix = GoWest(previousRolledMatrix);
+                //previousRolledMatrix = GoSouth(previousRolledMatrix);
+                //previousRolledMatrix = GoEast(previousRolledMatrix);
+            }
+            
+            
+
+            for (int i = 0; i < previousRolledMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < previousRolledMatrix.GetLength(1); j++)
+                {
+                    Console.Write(previousRolledMatrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+            
+            
+            return totalRockWeight.ToString();
+        }
+
+        private char[,] GoNorth(int rows, int columns, char[,] previousMatrix)
+        {
+            var columnsWithRocks = new Dictionary<int, int>();
+            for (int i = 0; i < columns; i++) {
+                columnsWithRocks.Add(i, 0);
+            }
+
+            var rolledMatrix = new char[rows, columns];
+            for (int i = rows - 1; i >= 0; i--) {
+                for (int j = 0; j < columns - 1; j++) {
+                    if (i == 0) {
+                        if (previousMatrix[i,j] == '.') {
+                            if (columnsWithRocks[j] == 0)
+                            {
+                                rolledMatrix[i, j] = previousMatrix[i,j];
+                            }
+                            else
+                            {
+                                ResolveStoredRocks(columnsWithRocks, rolledMatrix, i - 1, j, rows);
+                            }
+                            continue;
+                        }
+                        else
+                        {
+                            rolledMatrix[i, j] = previousMatrix[i,j];
+                            ResolveStoredRocks(columnsWithRocks, rolledMatrix, i, j, rows);
+                            continue;
+                        }
+                    }
+
+                    if (previousMatrix[i,j] == 'O')
+                    {
+                        // store rolled rock
+                        columnsWithRocks[j] += 1;
+                        // add empty space for now
+                        rolledMatrix[i, j] = '.';
+                        continue;
+                    }
+
+                    if (previousMatrix[i,j] == '#')
+                    {
+                        ResolveStoredRocks(columnsWithRocks, rolledMatrix, i, j, rows);
+                    }
+
+                    rolledMatrix[i, j] = previousMatrix[i,j];
+                }
+            }
+            return rolledMatrix;
+        }
+
+        private char[,] GetInitalMatrix(int rows, int columns)
+        {
+            var columnsWithRocks = new Dictionary<int, int>();
+            for (int i = 0; i < columns; i++) {
+                columnsWithRocks.Add(i, 0);
+            }
+            var rolledMatrix = new char[rows, columns];
+            for (int i = rows - 1; i >= 0; i--) {
+                var chars = Lines[i].ToCharArray();
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (chars[j] == '.')
+                        {
+                            if (columnsWithRocks[j] == 0)
+                            {
+                                rolledMatrix[i, j] = chars[j];
+                            }
+                            else
+                            {
+                                ResolveStoredRocks(columnsWithRocks, rolledMatrix, i - 1, j, rows);
+                            }
+                            continue;
+                        }
+                        else
+                        {
+                            rolledMatrix[i, j] = chars[j];
+                            ResolveStoredRocks(columnsWithRocks, rolledMatrix, i, j, rows);
+                            continue;
+                        }
+                    }
+
+                    if (chars[j] == 'O')
+                    {
+                        // store rolled rock
+                        columnsWithRocks[j] += 1;
+                        // add empty space for now
+                        rolledMatrix[i, j] = '.';
+                        continue;
+                    }
+
+                    if (chars[j] == '#')
+                    {
+                        ResolveStoredRocks(columnsWithRocks, rolledMatrix, i, j, rows);
+                    }
+
+                    rolledMatrix[i, j] = chars[j];
+                }
+            }
+            return rolledMatrix;
         }
     }
 }
